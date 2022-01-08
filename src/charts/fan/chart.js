812375bef36b70e5f8ac33dbde358c84f8ead452
Shreshth Mohan <sremog@gmail.com>
2022-01-09 00:26:47 +0530

    add grid, grid base, y axis

--- src/charts/fan/chart.js
+++ src/charts/fan/chart.js
@@ -1,16 +1,37 @@
 /* global d3 */
 
+function preventOverflow({ allComponents, svg, safetyMargin = 5, margins }) {
+  const { marginLeft, marginRight, marginTop, marginBottom } = margins
+  let allComponentsBox = allComponents.node().getBBox()
+
+  const updatedViewBoxWidth =
+    allComponentsBox.width + safetyMargin + marginLeft + marginRight
+  const updatedViewBoxHeight =
+    allComponentsBox.height + safetyMargin + marginTop + marginBottom
+  svg.attr('viewBox', `0 0 ${updatedViewBoxWidth} ${updatedViewBoxHeight}`)
+
+  allComponentsBox = allComponents.node().getBBox()
+
+  allComponents.attr(
+    'transform',
+    `translate(${-allComponentsBox.x + safetyMargin / 2 + marginLeft}, ${
+      -allComponentsBox.y + safetyMargin / 2 + marginTop
+    })`,
+  )
+}
+
 const radiusField = 'distance (km)'
 const angleField = 'price change (%)'
 const nameField = 'Route'
 
 // Options:
 const coreChartWidth = 1000
-const aspectRatio = 1
-const marginBottom = 100
+const aspectRatio = 2
+const marginBottom = 0
 const marginLeft = 0
-const marginRight = 100
-const marginTop = 300
+const marginRight = 0
+const marginTop = 0
+const bgColor = 'transparent'
 
 const chartContainerSelector = '#chart-container'
 
@@ -26,6 +47,7 @@ d3.csv('data.csv').then(rawData => {
   const svg = chartParent
     .append('svg')
     .attr('viewBox', `0 0 ${viewBoxWidth} ${viewBoxHeight}`)
+    .style('background', bgColor)
 
   const allComponents = svg.append('g').attr('class', 'all-components')
 
@@ -43,19 +65,24 @@ d3.csv('data.csv').then(rawData => {
   })
 
   const radiusScale = d3
-    .scaleRadial()
+    .scaleLinear()
+    .range([0, d3.min([coreChartHeight, coreChartWidth])])
     .domain([0, d3.max(data, d => d[radiusField])])
-    .range([0, 400])
+    .nice()
 
-  // what value corresponds to what angle
-  // 50 corresponds to pi/2 rad
   const angleScale = d3
     .scaleLinear()
     .domain([0, -50])
     .range([Math.PI / 2, Math.PI])
 
-  chartCore
+  const lines = chartCore
     .append('g')
+    .attr(
+      'transform',
+      `translate(${coreChartWidth / 2 - radiusScale.range()[1] / 2}, 0)`,
+    )
+
+  lines
     .selectAll('path')
     .data(data)
     .join('path')
@@ -73,5 +100,70 @@ d3.csv('data.csv').then(rawData => {
       console.log(d[nameField])
     })
 
+  lines
+    .append('g')
+    .call(d3.axisLeft(radiusScale).ticks(5).tickSize(0))
+    .call(g => g.select('.domain').remove())
+
+  const yGridLinesData = radiusScale.ticks()
+  // console.log(yGridLinesData)
+
+  lines
+    .append('g')
+    .lower()
+    .selectAll('path')
+    .data(yGridLinesData)
+    .join('path')
+    .attr('d', d =>
+      d3.arc()({
+        innerRadius: 0,
+        outerRadius: radiusScale(d),
+        startAngle: Math.PI / 2,
+        endAngle: Math.PI,
+      }),
+    )
+    .attr('fill', 'none')
+    .attr('stroke', 'white')
+    .attr('stroke-width', 2)
+
+  const xGridLinesData = angleScale.ticks(5)
+  console.log(xGridLinesData)
+
+  lines
+    .append('g')
+    .lower()
+    .selectAll('path')
+    .data(xGridLinesData)
+    .join('path')
+    .attr('d', d => {
+      return d3.lineRadial()([
+        [0, 0],
+        [angleScale(d), radiusScale.range()[1]],
+      ])
+    })
+    .attr('stroke', 'white')
+    .attr('stroke-width', 1.5)
+    // .attr('opacity', 0.7)
+    .attr('fill', 'none')
+
+  lines
+    .append('path')
+    .attr(
+      'd',
+      d3.arc()({
+        innerRadius: 0,
+        outerRadius: radiusScale.range()[1],
+        startAngle: Math.PI / 2,
+        endAngle: Math.PI,
+      }),
+    )
+    .attr('fill', '#ebece7')
+    .lower()
+
+  preventOverflow({
+    allComponents,
+    svg,
+    margins: { marginBottom, marginLeft, marginRight, marginTop },
+  })
   // console.log(data)
 })
