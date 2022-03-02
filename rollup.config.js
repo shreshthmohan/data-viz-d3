ab0476a4a040501a9389f9fafc6de86ef5cae517
Shreshth Mohan <sremog@gmail.com>
2022-03-02 11:49:23 +0530

    fix rollup

--- rollup.config.js
+++ rollup.config.js
@@ -2,28 +2,28 @@ import resolve from '@rollup/plugin-node-resolve'
 import { terser } from 'rollup-plugin-terser'
 
 export default [
-  // {
-  //   input: './src/fan/chart.js',
-  //   output: [
-  //     {
-  //       file: './dist/fan/chart.js',
-  //       format: 'umd',
-  //       name: 'fan', // use this global var when using in the browser
-  //     },
-  //   ],
-  //   plugins: [resolve(), terser()],
-  // },
-  // {
-  //   input: './src/taxes/chart.js',
-  //   output: [
-  //     {
-  //       file: './dist/taxes/chart.js',
-  //       format: 'umd',
-  //       name: 'taxes', // use this global var when using in the browser
-  //     },
-  //   ],
-  //   plugins: [resolve(), terser()],
-  // },
+  {
+    input: './src/fan/chart.js',
+    output: [
+      {
+        file: './dist/fan/chart.js',
+        format: 'umd',
+        name: 'fan', // use this global var when using in the browser
+      },
+    ],
+    plugins: [resolve(), terser()],
+  },
+  {
+    input: './src/taxes/chart.js',
+    output: [
+      {
+        file: './dist/taxes/chart.js',
+        format: 'umd',
+        name: 'taxes', // use this global var when using in the browser
+      },
+    ],
+    plugins: [resolve(), terser()],
+  },
   {
     input: './src/happiness/chart.js',
     output: [
